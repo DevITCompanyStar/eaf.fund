6432cb92691d3ffabffc89095c10969b2886203f
vladkleshko <vkoesko@gmail.com>
2025-06-24 13:38:52 +0300

    Created 'programs/', 'programs/<pk>/' & 'programs/categories/' urls

--- src/apps/programs/urls.py
+++ src/apps/programs/urls.py
@@ -1,3 +1,14 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import (
+    ProgramListView,
+    ProgramDetailView,
+    CategoryListView
+)
+
+urlpatterns = [
+    path("programs/", ProgramListView.as_view(), name="program-list"),
+    path("programs/<int:pk>/", ProgramDetailView.as_view(), name="program-detail"),
+    path("programs/categories/", CategoryListView.as_view(), name="category-list"),
+
+]
