c52d4db373b6af3ba593a83fd09619b4b34f0da0
vladkleshko <vkoesko@gmail.com>
2025-06-24 13:32:33 +0300

    Created admin panel for programs app & custom filter by category

--- src/apps/programs/admin.py
+++ src/apps/programs/admin.py
@@ -1,3 +1,58 @@
 from django.contrib import admin
+from nested_inline.admin import NestedStackedInline
 
-# Register your models here.
+from .models import (
+    Program,
+    Purpose,
+    Category,
+    HowWeWork,
+    ProgramImage,
+)
+
+
+class DecadeBornListFilter(admin.SimpleListFilter):
+    title = "Categories"
+    parameter_name = "category"
+
+    def lookups(self, request, model_admin):
+        return [
+            (category.slug, f"– {category.title}") for category in Category.objects.all()
+        ]
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(category__slug=self.value())
+        return queryset
+
+
+class PurposeImageInline(NestedStackedInline):
+    model = Purpose
+    extra = 0
+
+
+class HowWeWorkImageInline(NestedStackedInline):
+    model = HowWeWork
+    extra = 0
+
+
+class ProgramImageImageInline(NestedStackedInline):
+    model = ProgramImage
+    extra = 0
+
+
+@admin.register(Program)
+class ProgramAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "created_at")
+    search_fields = ("title", "description")
+    list_filter = (DecadeBornListFilter,)
+    inlines = (
+        PurposeImageInline,
+        HowWeWorkImageInline,
+        ProgramImageImageInline,
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("title", "slug", "created_at")
+    readonly_fields = ("slug",)
--- src/apps/programs/models.py
+++ src/apps/programs/models.py
@@ -1,3 +1,92 @@
 from django.db import models
+from unidecode import unidecode
+from django.utils.text import slugify
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(unique=True, max_length=255)
+    slug = models.SlugField(
+        unique=True,
+        blank=True,
+        max_length=255,
+        help_text="Auto generated from title"
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            translated_title = unidecode(self.title)
+            self.slug = slugify(translated_title)
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Program(models.Model):
+    title = models.CharField(max_length=255)
+    subtitle = models.TextField(null=True, blank=True)
+    support_text = models.TextField(null=True, blank=True)
+    description = models.TextField(null=True, blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="programs"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Program"
+        verbose_name_plural = "Programs"
+
+    def __str__(self):
+        return self.title
+
+
+class Purpose(models.Model):
+    program = models.ForeignKey(
+        Program,
+        on_delete=models.CASCADE,
+        related_name="purposes"
+    )
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+
+class HowWeWork(models.Model):
+    program = models.ForeignKey(
+        Program,
+        on_delete=models.CASCADE,
+        related_name="how_we_work"
+    )
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+
+
+class ProgramImage(models.Model):
+    report = models.ForeignKey(
+        Program,
+        on_delete=models.CASCADE,
+        related_name="images"
+    )
+    image = models.ImageField(upload_to="images/programs/")
+    title = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["created_at"]
+        verbose_name = "Image"
+        verbose_name_plural = "Images"
