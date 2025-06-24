59f14c722681ef126b869ce24e7d7e09523d3a1d
vladkleshko <vkoesko@gmail.com>
2025-06-24 13:37:34 +0300

    Created ProgramImageSerializer, PurposeSerializer, HowWeWorkSerializer, ProgramSerializer, ProgramDetailSerializer, CategorySerializer serializers

---
+++ src/apps/programs/serializers.py
@@ -0,0 +1,68 @@
+from rest_framework import serializers
+
+from .models import (
+    Program,
+    Purpose,
+    Category,
+    HowWeWork,
+    ProgramImage
+)
+
+
+class ProgramImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProgramImage
+        fields = ["image", "title"]
+
+
+class PurposeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Purpose
+        fields = ["description"]
+
+
+class HowWeWorkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = HowWeWork
+        fields = ["description"]
+
+
+class ProgramSerializer(serializers.ModelSerializer):
+    image = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Program
+        fields = [
+            "image",
+            "title",
+            "subtitle",
+        ]
+
+    def get_image(self, obj):
+        image = obj.images.first()
+        if image:
+            return image.image.url
+        return None
+
+
+class ProgramDetailSerializer(ProgramSerializer):
+    images = ProgramImageSerializer(many=True)
+    purposes = PurposeSerializer(many=True)
+    how_we_work = HowWeWorkSerializer(many=True)
+
+    class Meta:
+        model = Program
+        fields = [
+            "title",
+            "support_text",
+            "description",
+            "purposes",
+            "how_we_work",
+            "images",
+        ]
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["slug", "title"]
