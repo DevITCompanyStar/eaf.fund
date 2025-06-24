b6e4ec12f3fd0fb764e463fbe4b6d7de69c1def8
vladkleshko <vkoesko@gmail.com>
2025-06-24 16:07:19 +0300

    Created 'contacts/' & 'contacts/volunteer-applications/' & 'contacts/contact-messages/' urls

--- src/apps/contacts/urls.py
+++ src/apps/contacts/urls.py
@@ -1,3 +1,13 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import (
+    ContactListView,
+    VolunteerApplicationCreateView,
+    ContactMessageCreateView
+)
+
+urlpatterns = [
+    path("contacts/", ContactListView.as_view(), name="contact-list"),
+    path("contacts/volunteer-applications/", VolunteerApplicationCreateView.as_view(), name="volunteer-application-create"),
+    path("contacts/contact-messages/", ContactMessageCreateView.as_view(), name="contact-message-create"),
+]
