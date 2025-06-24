fb7901623da8aae502384969075b424e24ffaeeb
vladkleshko <vkoesko@gmail.com>
2025-06-24 16:05:01 +0300

    Created ContactListView, VolunteerApplicationCreateView & ContactMessageCreateView views

--- src/apps/contacts/admin.py
+++ src/apps/contacts/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Contact, VolunteerApplication, ContactMessage
+
+
+@admin.register(Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ("email", "phone_number", "location")
+
+
+@admin.register(VolunteerApplication)
+class VolunteerApplicationAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "last_name", "email", "phone_number", "submitted_at")
+    search_fields = ("first_name", "last_name", "email", "phone_number")
+    list_filter = ("submitted_at",)
+
+
+@admin.register(ContactMessage)
+class ContactMessageAdmin(admin.ModelAdmin):
+    list_display = ("first_name", "email", "phone_number", "message", "contacted_at")
+    search_fields = ("first_name", "email", "phone_number", "message")
+    list_filter = ("contacted_at",)
--- src/apps/contacts/views.py
+++ src/apps/contacts/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from .models import Contact, VolunteerApplication, ContactMessage
+from .serializers import (
+    ContactSerializer,
+    ContactMessageSerializer,
+    VolunteerApplicationSerializer
+)
+
+
+class ContactListView(generics.ListAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class VolunteerApplicationCreateView(generics.CreateAPIView):
+    queryset = VolunteerApplication.objects.all()
+    serializer_class = VolunteerApplicationSerializer
+
+
+class ContactMessageCreateView(generics.CreateAPIView):
+    queryset = ContactMessage.objects.all()
+    serializer_class = ContactMessageSerializer
