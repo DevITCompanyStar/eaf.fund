1b110208ab75517e13f455a7e00f3edd6cdbc32c
vladkleshko <vkoesko@gmail.com>
2025-06-24 10:22:18 +0300

    Added pagination for ReportListView

--- src/apps/reports/views.py
+++ src/apps/reports/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.pagination import LimitOffsetPagination
 
 from .models import Report
 from .serializers import ReportSerializer
@@ -7,6 +8,19 @@ from .serializers import ReportSerializer
 class ReportListView(generics.ListAPIView):
     queryset = Report.objects.all()
     serializer_class = ReportSerializer
+    pagination_class = LimitOffsetPagination
+
+    def list(self, request, *args, **kwargs):
+        paginator = self.pagination_class()
+        paginator.default_limit = self.request.query_params.get("limit", 10)
+        result = paginator.paginate_queryset(self.get_queryset(), request)
+        serializer = self.get_serializer(
+            instance=result,
+            many=True,
+            context={"request": request}
+        )
+
+        return paginator.get_paginated_response(serializer.data)
 
 
 class ReportDetailView(generics.RetrieveAPIView):
