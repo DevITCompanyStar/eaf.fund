fa8c9c68e523f24dd1232a84454f8eb3e8c3a3a1
vladkleshko <vkoesko@gmail.com>
2025-06-25 09:31:05 +0300

    Created PaymentsOptionView models

--- src/apps/payments/models.py
+++ src/apps/payments/models.py
@@ -1,3 +1,18 @@
+from ckeditor.fields import RichTextField
 from django.db import models
 
-# Create your models here.
+
+class PaymentOption(models.Model):
+    title = models.CharField(max_length=100, blank=True, null=True)
+    description = RichTextField(blank=True, null=True)
+    currency = models.CharField(max_length=50, blank=True, null=True)
+    details = models.CharField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+        verbose_name = "Payment Method"
+        verbose_name_plural = "Payment Methods"
+
+    def __str__(self):
+        return self.title
