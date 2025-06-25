b28611d67405ae9a016568d4229ea6608ad21fd8
vladkleshko <vkoesko@gmail.com>
2025-06-25 09:31:36 +0300

    Created admin panel for payments app

--- src/apps/payments/admin.py
+++ src/apps/payments/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import PaymentOption
+
+
+@admin.register(PaymentOption)
+class PaymentMethodAdmin(admin.ModelAdmin):
+    list_display = ("title", "description", "currency", "details")
