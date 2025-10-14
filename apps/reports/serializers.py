2d52991669c2412e9783f8bb45ca32d6fd92f799
vladkleshko <vkoesko@gmail.com>
2025-10-14 13:09:36 +0300

    create ReportVideoSerializer serializer

--- apps/reports/serializers.py
+++ apps/reports/serializers.py
@@ -1,16 +1,23 @@
 from rest_framework import serializers
 
-from .models import Report, ReportImage
+from .models import Report, ReportImage, ReportVideo
 
 
 class ReportImageSerializer(serializers.ModelSerializer):
     class Meta:
         model = ReportImage
-        fields = ["image", "title"]
+        fields = ["image"]
+
+
+class ReportVideoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ReportVideo
+        fields = ["video"]
 
 
 class ReportSerializer(serializers.ModelSerializer):
     images = ReportImageSerializer(many=True)
+    videos = ReportVideoSerializer(many=True)
 
     class Meta:
         model = Report
@@ -19,5 +26,6 @@ class ReportSerializer(serializers.ModelSerializer):
             "title",
             "description",
             "images",
+            "videos",
             "event_date"
         ]
