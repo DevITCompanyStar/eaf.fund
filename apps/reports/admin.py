170c4f55899feb2741fac692ba8531b676ac3655
vladkleshko <vkoesko@gmail.com>
2025-10-14 13:09:52 +0300

    create ReportVideoInline

--- apps/reports/admin.py
+++ apps/reports/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from nested_inline.admin import NestedStackedInline
 
-from .models import Report, ReportImage
+from .models import Report, ReportImage, ReportVideo
 
 
 class ReportImageInline(NestedStackedInline):
     model = ReportImage
+    extra = 0
+
+class ReportVideoInline(NestedStackedInline):
+    model = ReportVideo
     extra = 0
 
 
@@ -14,4 +18,4 @@ class ReportAdmin(admin.ModelAdmin):
     list_display = ("title", "description", "event_date")
     search_fields = ("title", "description")
     list_filter = ("event_date",)
-    inlines = [ReportImageInline]
+    inlines = [ReportImageInline, ReportVideoInline]
